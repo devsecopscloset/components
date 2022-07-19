62482536614b07bd7dfb4b86f17114890a8d3c4f
devsecopscloset <106809417+devsecopscloset@users.noreply.github.com>
2022-07-19 15:42:48 +0530

    Updated for multiple pipelines

--- dev/trigger.sh
+++ dev/trigger.sh
@@ -10,11 +10,4 @@ then
 	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:dev
 fi
 
-if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_start` ]]
-then
-	docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d devsecopscloset/closet_deploy:dev
-elif [[ `docker ps -a -f status=exited | grep closet_start` ]]
-then
-	docker rm -f closet_start &&
-	docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d devsecopscloset/closet_deploy:dev
-fi
+docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube -d devsecopscloset/closet_deploy:dev
