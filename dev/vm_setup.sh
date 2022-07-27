95f6040c23922d68858871b3802e48abe8ed93ea
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-07-27 11:00:43 +0530

    adding replicas of application and loadbalancer config to nginx

--- dev/vm_setup.sh
+++ dev/vm_setup.sh
@@ -1,5 +1,5 @@
 sudo apt update &&
 sudo apt upgrade -y &&
-sudo apt install -y docker.io ddclient &&
+sudo apt install -y docker.io ddclient docker-compose &&
 sudo usermod -aG docker $USER
 # COPY DDCLIENT CONFIG
