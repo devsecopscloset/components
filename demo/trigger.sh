045b2a7089b4fac059885d713c3f4cb85d11db52
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-07-29 11:10:09 +0530

    demo test with dev builds

--- demo/trigger.sh
+++ demo/trigger.sh
@@ -2,19 +2,19 @@
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_dd` ]]
 then 
-	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo:demo
+	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo:dev
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_octant` ]]
 then
-	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:demo
+	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:dev
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_start` ]]
 then
-	docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d devsecopscloset/closet_deploy
+	docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d devsecopscloset/closet_deploy:dev
 elif [[ `docker ps -a -f status=exited | grep closet_start` ]]
 then
 	docker rm -f closet_start &&
-	docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d devsecopscloset/closet_deploy
+	docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d devsecopscloset/closet_deploy:dev
 fi
