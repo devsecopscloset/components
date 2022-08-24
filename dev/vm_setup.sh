c62a9eea07fe3e9a9911dfccf27f734fd6163af0
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-08-24 10:48:11 +0530

    options correction

--- dev/vm_setup.sh
+++ dev/vm_setup.sh
@@ -15,91 +15,95 @@ echo -e "\n3. Docker Compose Install (dc)"
 echo -e "\n4. Docker User Configuration (du)"
 echo -e "\n5. DDCLIENT configuration (dd)"
 echo -e "\n5. All (a)\n"
-echo -e "\e[1;31mEnter the option: \e[0m"
+echo -e "\e[1;31mEnter the option: (Enter mutiple options separated by space) \e[0m"
 read OPTION
- 
-###### DOCKER CONFIGURATION #######
-if [ "$OPTION" = "d" ] || [ "$OPTION" = "a" ]; then
-    echo -e "\n\e[1;32m********* DOCKER SELECTED *********\e[0m"
-    if [ -x "$(command -v docker)" ]; then
-        echo -e "\n\e[1;42m\e[1;31m********* DOCKER IS INSTALLED *********\e[1;0m"
+
+for i in $OPTION; do
+
+    ###### DOCKER CONFIGURATION #######
+    if [ "$i" = "d" ] || [ "$i" = "a" ]; then
+        echo -e "\n\e[1;32m********* DOCKER SELECTED *********\e[0m"
+        if [ -x "$(command -v docker)" ]; then
+            echo -e "\n\e[1;42m\e[1;31m********* DOCKER IS INSTALLED *********\e[1;0m"
+        else
+            echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER *********\e[1;0m"
+            apt-get -qq install -y docker.io &&
+        fi
     else
-        echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER *********\e[1;0m"
-        apt-get -qq install -y docker.io &&
+        echo -e "\n\e[1;31m********* DOCKER NOT CHOSEN *********\e[0m"
     fi
-else
-    echo -e "\n\e[1;31m********* DOCKER NOT CHOSEN *********\e[0m"
-fi
 
-###### DOCKER USER GROUP CONFIGURATION #######
+    ###### DOCKER USER GROUP CONFIGURATION #######
 
-if [ "$OPTION" = "d" ] || [ "$OPTION" = "a" ] || [ "$OPTION" = "du" ]; then
-    echo -e "\n\e[1;32m********* DOCKER USER CONFIG SELECTED *********\e[0m"
-    echo -e "\e[1;31mENTER USERNAME: \e[0m"
-    read USERNAME
-    if ! (( $(grep '^docker' /etc/group | grep -c $USERNAME) == 0 )); then
-        echo -e "\n\e[1;44m\e[1;31m********* USER IS ALREADY PART OF DOCKER GROUP *********\e[1;0m"
+    if [ "$i" = "d" ] || [ "$i" = "a" ] || [ "$i" = "du" ]; then
+        echo -e "\n\e[1;32m********* DOCKER USER CONFIG SELECTED *********\e[0m"
+        echo -e "\e[1;31mENTER USERNAME: \e[0m"
+        read USERNAME
+        if ! (( $(grep '^docker' /etc/group | grep -c $USERNAME) == 0 )); then
+            echo -e "\n\e[1;44m\e[1;31m********* USER IS ALREADY PART OF DOCKER GROUP *********\e[1;0m"
+        else
+            echo -e "\n\e[1;44m\e[1;31m********* ADDING $USERNAME TO DOCKER GROUP *********\e[1;0m"
+            usermod -aG docker $USERNAME
+        fi
     else
-        echo -e "\n\e[1;44m\e[1;31m********* ADDING $USERNAME TO DOCKER GROUP *********\e[1;0m"
-        usermod -aG docker $USERNAME
+        echo -e "\n\e[1;31m********* DOCKER NOT CHOSEN *********\e[0m"
     fi
-else
-    echo -e "\n\e[1;31m********* DOCKER NOT CHOSEN *********\e[0m"
-fi
 
 
-###### DOCKER-COMPOSE CONFIGURATION #######
-if [ "$OPTION" = "dc" ] || [ "$OPTION" = "a" ]; then
-    echo -e "\n\e[1;32m********* DOCKER-COMPOSE SELECTED *********\e[0m"
-    if [ -x "$(command -v docker-compose)" ]; then
-        echo -e "\n\e[1;42m\e[1;31m********* DOCKER-COMPOSE IS INSTALLED *********\e[1;0m"
+    ###### DOCKER-COMPOSE CONFIGURATION #######
+    if [ "$i" = "dc" ] || [ "$i" = "a" ]; then
+        echo -e "\n\e[1;32m********* DOCKER-COMPOSE SELECTED *********\e[0m"
+        if [ -x "$(command -v docker-compose)" ]; then
+            echo -e "\n\e[1;42m\e[1;31m********* DOCKER-COMPOSE IS INSTALLED *********\e[1;0m"
+        else
+            echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER-COMPOSE *********\e[1;0m"
+            apt-get -qq install -y docker-compose
+        fi
     else
-        echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER-COMPOSE *********\e[1;0m"
-        apt-get -qq install -y docker-compose
+        echo -e "\n\e[1;31m********* DOCKER-COMPOSE NOT CHOSEN *********\e[0m"
     fi
-else
-    echo -e "\n\e[1;31m********* DOCKER-COMPOSE NOT CHOSEN *********\e[0m"
-fi
 
-###### KUBECTL CONFIGURATION #######
-if [ "$OPTION" = "k" ] || [ "$OPTION" = "a" ]; then
-    echo -e "\n\e[1;32m********* KUBECTL SELECTED *********\e[0m"
-    if [ -x "$(command -v kubectl)" ]; then
-        echo -e "\n\e[1;42m\e[1;31m********* KUBECTL IS INSTALLED *********\e[1;0m"
+    ###### KUBECTL CONFIGURATION #######
+    if [ "$i" = "k" ] || [ "$i" = "a" ]; then
+        echo -e "\n\e[1;32m********* KUBECTL SELECTED *********\e[0m"
+        if [ -x "$(command -v kubectl)" ]; then
+            echo -e "\n\e[1;42m\e[1;31m********* KUBECTL IS INSTALLED *********\e[1;0m"
+        else
+            echo -e "\n\e[1;42m\e[1;31m********* INSTALLING KUBECTL *********\e[1;0m" &&
+            curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
+            install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
+        fi
     else
-        echo -e "\n\e[1;42m\e[1;31m********* INSTALLING KUBECTL *********\e[1;0m" &&
-        curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
-        install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl &&
+        echo -e "\n\e[1;31m********* KUBECTL NOT CHOSEN *********\e[0m"
     fi
-else
-    echo -e "\n\e[1;31m********* KUBECTL NOT CHOSEN *********\e[0m"
-fi
 
-###### DDCLIENT CONFIGURATION #######
-if [ "$OPTION" = "dd" ] || [ "$OPTION" = "a" ]; then
-    echo -e "\n\e[1;32m********* DDCLIENT SELECTED *********\e[0m"
-    if [ -x "$(command -v ddclient)" ]; then
-        echo -e "\n\e[1;42m\e[1;31m********* DDCLIENT IS INSTALLED *********\e[1;0m"
+    ###### DDCLIENT CONFIGURATION #######
+    if [ "$i" = "dd" ] || [ "$i" = "a" ]; then
+        echo -e "\n\e[1;32m********* DDCLIENT SELECTED *********\e[0m"
+        if [ -x "$(command -v ddclient)" ]; then
+            echo -e "\n\e[1;42m\e[1;31m********* DDCLIENT IS INSTALLED *********\e[1;0m"
+        else
+            apt-get -qq install -y ddclient &&
+            mkdir /etc/ddclient &&
+            echo -e "\n\e[1;42m\e[1;31m********* DOWNLOADING CONFIG *********\e[1;0m" &&
+            # curl -s https://raw.githubusercontent.com/devsecopscloset/components/master/dev/ddclient.conf -o /etc/ddclient/ddclient.conf &&
+            # curl -s https://raw.githubusercontent.com/ddclient/ddclient/develop/sample-etc_rc.d_init.d_ddclient.ubuntu -o /etc/systemd/system/ddclient.service &&
+            # read -p 'Enter the username: ' username &&
+            # read -p 'Enter the password: ' password &&
+            # read -p 'Enter the domain names in comma separated values : ( example.com, example2.com ) ' domains &&
+            echo -e '\n\e[1;42m\e[1;31m********* CREATING CONFIGURATION FOR DDCLIENT *********\e[1;0m' &&
+            # echo 'username= '$username >> /etc/ddclient/ddclient.conf &&
+            # echo 'password= '$password >> /etc/ddclient/ddclient.conf &&
+            # echo $domains >> /etc/ddclient/ddclient.conf &&
+            echo -e "\n\e[1;42m\e[1;31m********* ENABLING DDCLIENT ON STARTUP *********\e[1;0m" &&
+            systemctl enable ddclient.service &&
+            echo -e "\n\e[1;42m\e[1;31m********* STARTING DDCLIENT SERVICE *********\e[1;0m"
+            systemctl start ddclient.service
+        fi
     else
-        apt-get -qq install -y ddclient &&
-        mkdir /etc/ddclient &&
-        echo -e "\n\e[1;42m\e[1;31m********* DOWNLOADING CONFIG *********\e[1;0m" &&
-        # curl -s https://raw.githubusercontent.com/devsecopscloset/components/master/dev/ddclient.conf -o /etc/ddclient/ddclient.conf &&
-        # curl -s https://raw.githubusercontent.com/ddclient/ddclient/develop/sample-etc_rc.d_init.d_ddclient.ubuntu -o /etc/systemd/system/ddclient.service &&
-        # read -p 'Enter the username: ' username &&
-        # read -p 'Enter the password: ' password &&
-        # read -p 'Enter the domain names in comma separated values : ( example.com, example2.com ) ' domains &&
-        echo -e '\n\e[1;42m\e[1;31m********* CREATING CONFIGURATION FOR DDCLIENT *********\e[1;0m' &&
-        # echo 'username= '$username >> /etc/ddclient/ddclient.conf &&
-        # echo 'password= '$password >> /etc/ddclient/ddclient.conf &&
-        # echo $domains >> /etc/ddclient/ddclient.conf &&
-        echo -e "\n\e[1;42m\e[1;31m********* ENABLING DDCLIENT ON STARTUP *********\e[1;0m" &&
-        systemctl enable ddclient.service &&
-        echo -e "\n\e[1;42m\e[1;31m********* STARTING DDCLIENT SERVICE *********\e[1;0m"
-        systemctl start ddclient.service
+        echo -e "\n\e[1;31m********* DDCLIENT NOT CHOSEN *********\e[0m"
     fi
-else
-    echo -e "\n\e[1;31m********* DDCLIENT NOT CHOSEN *********\e[0m"
-fi
+
+done
 
 echo -e "\n\e[1;41m\e[1;32m********* FINISHED SETTING UP *********\e[1;0m"
