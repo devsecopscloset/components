b20844cda9cae09e8a1d6a8eb6c2b8f9c9107501
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-08-24 11:58:10 +0530

    added functions

--- dev/vm_setup.sh
+++ dev/vm_setup.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
 
-###### SYSTEM BASE PACKAGES INSTALL #######
+###### INSTALLATION FUNCTION ######
 
-echo -e '\nINSTALLING BASIC PACKAGES'
-apt-get -qq update &&
-apt-get -qq upgrade -y &&
-apt-get -qq install -y curl git make perl gcc openjdk-11-jdk &&
+docker () {
+    echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER *********\e[1;0m"
+    apt-get -qq install -y docker.io
+}
+
+dockercompose () {
+    echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER-COMPOSE *********\e[1;0m"
+    apt-get -qq install -y docker-compose
+}
+
+dockeruser () {
+    echo -e "\n\e[1;44m\e[1;31m********* ADDING $USERNAME TO DOCKER GROUP *********\e[1;0m"
+    usermod -aG docker $USERNAME
+}
+
+kubectl () {
+    echo -e "\n\e[1;42m\e[1;31m********* INSTALLING KUBECTL *********\e[1;0m" &&
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+}
+
+ddclient () {
+    apt-get -qq install -y ddclient &&
+    mkdir /etc/ddclient &&
+    echo -e "\n\e[1;42m\e[1;31m********* DOWNLOADING CONFIG *********\e[1;0m" &&
+    curl -s https://raw.githubusercontent.com/devsecopscloset/components/master/dev/ddclient.conf -o /etc/ddclient/ddclient.conf &&
+    curl -s https://raw.githubusercontent.com/ddclient/ddclient/develop/sample-etc_rc.d_init.d_ddclient.ubuntu -o /etc/systemd/system/ddclient.service &&
+    read -p 'Enter the username: ' username &&
+    read -p 'Enter the password: ' password &&
+    read -p 'Enter the domain names in comma separated values : ( example.com, example2.com ) ' domains &&
+    echo -e '\n\e[1;42m\e[1;31m********* CREATING CONFIGURATION FOR DDCLIENT *********\e[1;0m' &&
+    echo 'username= '$username >> /etc/ddclient/ddclient.conf &&
+    echo 'password= '$password >> /etc/ddclient/ddclient.conf &&
+    echo $domains >> /etc/ddclient/ddclient.conf &&
+    echo -e "\n\e[1;42m\e[1;31m********* ENABLING DDCLIENT ON STARTUP *********\e[1;0m" &&
+    systemctl enable ddclient.service &&
+    echo -e "\n\e[1;42m\e[1;31m********* STARTING DDCLIENT SERVICE *********\e[1;0m"
+    systemctl start ddclient.service
+}
+
+baseinstall () {
+    echo -e '\nINSTALLING BASIC PACKAGES'
+    apt-get -qq update &&
+    apt-get -qq upgrade -y &&
+    apt-get -qq install -y curl git make perl gcc openjdk-11-jdk
+}
+
 
 
 echo -e "\nPLEASE SELECT THE COMPONENTS TO BE INSTALLED"
@@ -26,11 +69,8 @@ for i in $OPTION; do
         if [ -x "$(command -v docker)" ]; then
             echo -e "\n\e[1;42m\e[1;31m********* DOCKER IS INSTALLED *********\e[1;0m"
         else
-            echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER *********\e[1;0m"
-            apt-get -qq install -y docker.io
+            docker
         fi
-    else
-        echo -e "\n\e[1;31m********* DOCKER NOT CHOSEN *********\e[0m"
     fi
 
     ###### DOCKER USER GROUP CONFIGURATION #######
@@ -42,13 +82,9 @@ for i in $OPTION; do
         if ! (( $(grep '^docker' /etc/group | grep -c $USERNAME) == 0 )); then
             echo -e "\n\e[1;44m\e[1;31m********* USER IS ALREADY PART OF DOCKER GROUP *********\e[1;0m"
         else
-            echo -e "\n\e[1;44m\e[1;31m********* ADDING $USERNAME TO DOCKER GROUP *********\e[1;0m"
-            usermod -aG docker $USERNAME
+            dockeruser
         fi
-    else
-        echo -e "\n\e[1;31m********* DOCKER NOT CHOSEN *********\e[0m"
     fi
-
 
     ###### DOCKER-COMPOSE CONFIGURATION #######
     if [ "$i" = "dc" ] || [ "$i" = "a" ]; then
@@ -56,11 +92,8 @@ for i in $OPTION; do
         if [ -x "$(command -v docker-compose)" ]; then
             echo -e "\n\e[1;42m\e[1;31m********* DOCKER-COMPOSE IS INSTALLED *********\e[1;0m"
         else
-            echo -e "\n\e[1;42m\e[1;31m********* INSTALLING DOCKER-COMPOSE *********\e[1;0m"
-            apt-get -qq install -y docker-compose
+            dockercompose
         fi
-    else
-        echo -e "\n\e[1;31m********* DOCKER-COMPOSE NOT CHOSEN *********\e[0m"
     fi
 
     ###### KUBECTL CONFIGURATION #######
@@ -69,12 +102,8 @@ for i in $OPTION; do
         if [ -x "$(command -v kubectl)" ]; then
             echo -e "\n\e[1;42m\e[1;31m********* KUBECTL IS INSTALLED *********\e[1;0m"
         else
-            echo -e "\n\e[1;42m\e[1;31m********* INSTALLING KUBECTL *********\e[1;0m" &&
-            curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" &&
-            install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+            kubectl
         fi
-    else
-        echo -e "\n\e[1;31m********* KUBECTL NOT CHOSEN *********\e[0m"
     fi
 
     ###### DDCLIENT CONFIGURATION #######
@@ -83,25 +112,8 @@ for i in $OPTION; do
         if [ -x "$(command -v ddclient)" ]; then
             echo -e "\n\e[1;42m\e[1;31m********* DDCLIENT IS INSTALLED *********\e[1;0m"
         else
-            apt-get -qq install -y ddclient &&
-            mkdir /etc/ddclient &&
-            echo -e "\n\e[1;42m\e[1;31m********* DOWNLOADING CONFIG *********\e[1;0m" &&
-            curl -s https://raw.githubusercontent.com/devsecopscloset/components/master/dev/ddclient.conf -o /etc/ddclient/ddclient.conf &&
-            curl -s https://raw.githubusercontent.com/ddclient/ddclient/develop/sample-etc_rc.d_init.d_ddclient.ubuntu -o /etc/systemd/system/ddclient.service &&
-            read -p 'Enter the username: ' username &&
-            read -p 'Enter the password: ' password &&
-            read -p 'Enter the domain names in comma separated values : ( example.com, example2.com ) ' domains &&
-            echo -e '\n\e[1;42m\e[1;31m********* CREATING CONFIGURATION FOR DDCLIENT *********\e[1;0m' &&
-            echo 'username= '$username >> /etc/ddclient/ddclient.conf &&
-            echo 'password= '$password >> /etc/ddclient/ddclient.conf &&
-            echo $domains >> /etc/ddclient/ddclient.conf &&
-            echo -e "\n\e[1;42m\e[1;31m********* ENABLING DDCLIENT ON STARTUP *********\e[1;0m" &&
-            systemctl enable ddclient.service &&
-            echo -e "\n\e[1;42m\e[1;31m********* STARTING DDCLIENT SERVICE *********\e[1;0m"
-            systemctl start ddclient.service
+            ddclient
         fi
-    else
-        echo -e "\n\e[1;31m********* DDCLIENT NOT CHOSEN *********\e[0m"
     fi
 
 done
