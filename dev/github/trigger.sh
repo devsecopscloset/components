71890ad473e27d15d3474dacbb7165769af6e536
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-09-02 10:44:38 +0530

    fixed image name error

--- dev/github/trigger.sh
+++ dev/github/trigger.sh
@@ -6,7 +6,7 @@ sed -i 's/DD_PORT:-8080/DD_PORT:-8000/g' docker-compose.yml &&
 /bin/bash dc-up-d.sh mysql-rabbitmq &&
 # rm -rf ~/django-DefectDojo &&
 
-if [ ! docker ps | grep `docker image | grep IMAGE_ID | awk '{print $1":"$2}'` | awk '{print $1}' ]
+if [ ! docker ps | grep `docker image | grep devsecopscloset/octant | awk '{print $1":"$2}'` | awk '{print $1}' ]
 then
 	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 --restart always -d devsecopscloset/octant:dev
 fi
